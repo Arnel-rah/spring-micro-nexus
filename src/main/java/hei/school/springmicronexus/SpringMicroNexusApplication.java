9cc55a577ea8be39aadd8eb70e2f59af3264fdf5
Arnel-rah <arnel.raharinandrasana@student.passerellesnumeriques.org>
2026-04-07 19:44:21 +0300

    feat: initial commit with spring boot and postgres driver

---
+++ src/main/java/hei/school/springmicronexus/SpringMicroNexusApplication.java
@@ -0,0 +1,13 @@
+package hei.school.springmicronexus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroNexusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroNexusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hei/school/springmicronexus/SpringMicroNexusApplicationTests.java
@@ -0,0 +1,13 @@
+package hei.school.springmicronexus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroNexusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
